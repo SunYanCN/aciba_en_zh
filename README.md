c054c975dd28b6bcc0dcf1210d3fa535e0b13002
SunYan <42198591+SunYanCN@users.noreply.github.com>
2019-12-23 19:20:58 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## aciba_en_zh
 中英文翻译数据集，来自金山词霸每日一句
-数据已经去重，总共2400条，更新至2019-3-21
+数据已经去重，总共2650条，更新至2019-12-23
 ## Reference
 - http://open.iciba.com/?c=wiki
 - https://github.com/malone6/Aiciba/blob/master/aciba.py
--- make_dataset.py
+++ make_dataset.py
@@ -1,28 +1,32 @@
-import requests
 import json
-from datetime import datetime,timedelta
-from tqdm import tqdm
 import re
+from datetime import datetime, timedelta
+
+import requests
+from tqdm import tqdm
+
 
 def clean(s):
     return re.sub(u"\\(.*?\\)|\\{.*?}|\\[.*?]|\\<.*?>|\\《.*?》|\\（.*?）", "", s)
 
-def dateRange(startDate,endDate):
+
+def dateRange(startDate, endDate):
     dates = []
-    date_tmp = datetime.strptime(startDate,"%Y-%m-%d")
-    end_date = datetime.strptime(endDate,"%Y-%m-%d")
+    date_tmp = datetime.strptime(startDate, "%Y-%m-%d")
+    end_date = datetime.strptime(endDate, "%Y-%m-%d")
     while date_tmp <= end_date:
         dates.append(date_tmp.strftime("%Y-%m-%d"))
         date_tmp += timedelta(days=1)
     return dates
 
-def make_dataset(url,dates):
+
+def make_dataset(url, dates):
     content = []
     note = []
 
     for date in tqdm(dates):
-        param = {'date':date}
-        r = requests.get(url,params=param)
+        param = {'date': date}
+        r = requests.get(url, params=param)
         data = json.loads(r.text)
 
         if clean(data["content"]) in content:
@@ -31,11 +35,11 @@ def make_dataset(url,dates):
             content.append(clean(data["content"]))
             note.append(clean(data["note"]))
 
-
-    with open("aciba_en_zh.txt",'a') as f:
+    with open("aciba_en_zh.txt", 'a') as f:
         for i, _ in enumerate(content):
-            f.write(content[i]+'\t\t'+note[i])
+            f.write(content[i] + '\t\t' + note[i])
             f.write('\n')
+
 
 def load_dataset(file):
     EN = []
@@ -43,15 +47,15 @@ def load_dataset(file):
     with open(file, 'r') as f:
         line = f.readline()
         line = line.strip('\n')
-        en, cn= line.split(sep='\t\t')
+        en, cn = line.split(sep='\t\t')
         EN.append(en)
         CN.append(cn)
-    return EN,CN
+    return EN, CN
+
 
 if __name__ == '__main__':
     url = 'http://open.iciba.com/dsapi/'
-    dates = dateRange('2012-01-01', '2019-3-21')
-    make_dataset(url,dates)
+    dates = dateRange('2012-01-01', '2019-12-23')
+    make_dataset(url, dates)
     EN, CN = load_dataset('aciba_en_zh.txt')
-    print(EN[0],CN[0])
-
+    print(EN[0], CN[0])
